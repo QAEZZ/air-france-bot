2078beb4f028770c174e5685d87a51310ccf84c7
QAEZZ <russian.devv@gmail.com>
2022-11-29 14:57:11 -0600

    flight plan all done

--- src/cogs/flight-plan.py
+++ src/cogs/flight-plan.py
@@ -6,8 +6,6 @@ from discord import ui
 from discord.ext import commands
 from discord.ui import View, Button
 
-global fpinteraction
-global fpmessage
 
 """
 Callsign: AirFrans-4633
@@ -30,10 +28,25 @@ class updateSquawkModal(ui.Modal, title="Update Squawk Code"):
     async def on_submit(self, interaction: discord.Interaction):
         try:
 
+            fpembed.description=f"""```diff
+- updated -```"""
             fpembed.remove_field(index=3)
-            fpembed.add_field(name="Squawk", value=f"```{self.squawk}```", inline=True)
+            fpembed.remove_footer()
+            if str(self.squawk) == "7500":
+                embed: discord.Embed = discord.Embed(
+                    title="🚨 HIJACKING IN PROGRESS 🚨",
+                    description="<@&975889011044409374>",
+                    color=discord.Color.from_rgb(47, 49, 54)
+                )
+                fpembed.add_field(name="Squawk", value=f"""```diff
+- {self.squawk} -```""", inline=True)
+                await interaction.response.send_message(embeds=[fpembed, embed])
+            else:
+                fpembed.add_field(name="Squawk", value=f"""```py
+{self.squawk}```""", inline=True)
 
-            await interaction.response.send_message(embed=fpembed)
+                await interaction.response.send_message(embed=fpembed)
+
         except Exception as e:
             print(e)
 
@@ -44,7 +57,7 @@ class fpModal(ui.Modal, title="Flight Plan Dialog"):
                             placeholder="Fromat: Callsign Aircraft. Ex. AirFrans-2241 B777", required=True, min_length=1)
         
         route = ui.TextInput(label="Route", style=discord.TextStyle.short,
-                            placeholder="Ex. GPD Direct. Ex2. EASTN ENTEX WEISN", required=True)
+                            placeholder="Ex. GPS Direct. Ex2. EASTN ENTEX WEISN", required=True)
         
         flight_rules = ui.TextInput(label="IFR/VFR and FL", style=discord.TextStyle.short,
                             placeholder="Format: IFR/VFR FL000. Ex. IFR FL050", required=True)
@@ -57,22 +70,27 @@ class fpModal(ui.Modal, title="Flight Plan Dialog"):
 
     except Exception as e:
         print(e)
-    # global fpinteraction
-    async def on_submit(self, fpinteraction: discord.Interaction):
+    # global interaction
+    async def on_submit(self, interaction: discord.Interaction):
         try:
 
             async def update_squawk_callback(fpinteraction):
-                fpmessage = await fpinteraction.response.send_modal(updateSquawkModal())
+                await interaction.delete_original_response()
+                await fpinteraction.response.send_modal(updateSquawkModal())
 
             global fpembed
 
             fpembed = discord.Embed(
                 title=f"Flight Plan for {self.callsign_aircraft}", color=discord.Color.from_rgb(47, 49, 54))
             
-            fpembed.add_field(name="Flight Rules and Flight Level", value=f"```{self.flight_rules}```", inline=True)
-            fpembed.add_field(name="Departing and Arriving Airports", value=f"```{self.airports}```", inline=True)
-            fpembed.add_field(name="Route", value=f"```{self.route}```", inline=True)
-            fpembed.add_field(name="Squawk Code", value=f"```{self.squawk}```", inline=True)
+            fpembed.add_field(name="Flight Rules and Flight Level", value=f"""```fix
+{self.flight_rules}```""", inline=True)
+            fpembed.add_field(name="Departing and Arriving Airports", value=f"""```prolog
+{self.airports}```""", inline=True)
+            fpembed.add_field(name="Route", value=f"""```fix
+{self.route}```""", inline=True)
+            fpembed.add_field(name="Squawk Code", value=f"""```py
+{self.squawk}```""", inline=True)
             fpembed.set_footer(text="Time out to update the squawk code is 15 minutes")
 
             update_squawk = Button(label="Update squawk code", style=discord.ButtonStyle.blurple)
@@ -83,7 +101,7 @@ class fpModal(ui.Modal, title="Flight Plan Dialog"):
 
             view.add_item(update_squawk)
 
-            await fpinteraction.response.send_message(embed=fpembed, view=view)
+            msg = await interaction.response.send_message(embed=fpembed, view=view)
         except Exception as e:
             print(e)
 
